206b74b2946a075c69b1790e134c70efe383e8b5
Arthur Lucas <arrtxweb@gmail.com>
2026-02-13 22:57:26 -0300

    fix: atualizar título e descrição do site para o Congresso de Mulheres

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sistema de Inscrição MW",
-  description: "Sistema de inscrição com pagamento via Mercado Pago",
+  title: "Cngresso de Mulheres",
+  description: "Site do Congresso de Mulheres",
 };
 
 export default function RootLayout({
